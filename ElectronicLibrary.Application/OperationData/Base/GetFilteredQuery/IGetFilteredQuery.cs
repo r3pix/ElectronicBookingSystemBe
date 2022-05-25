ec07857ebda732064c1357dcd0fb289cbe99c47e
r3pix <peterson2000@interia.pl>
2022-05-25 15:51:55 +0200

    Update IGetFilteredQuery.cs

--- ElectronicLibrary.Application/OperationData/Base/GetFilteredQuery/IGetFilteredQuery.cs
+++ ElectronicLibrary.Application/OperationData/Base/GetFilteredQuery/IGetFilteredQuery.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ElectronicLibrary.Infrastructure.Extensions;
+using MediatR;
 
 namespace ElectronicLibrary.Application.OperationData.Base.GetFilteredQuery
 {
-    internal interface IGetFilteredQuery
+    public interface IGetFilteredQuery<TKey> : ISelectQueryModel,IRequest<Response<List<SelectModel<TKey>>>> where TKey : struct
     {
     }
 }
