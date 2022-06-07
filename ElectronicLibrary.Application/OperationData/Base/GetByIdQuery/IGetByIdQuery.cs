5ff7c35697ea72ab1182fc2917bc2878f528b38b
r3pix <peterson2000@interia.pl>
2022-06-07 10:34:49 +0200

    get by id added

---
+++ ElectronicLibrary.Application/OperationData/Base/GetByIdQuery/IGetByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectronicLibrary.Infrastructure.Extensions;
+using MediatR;
+
+namespace ElectronicLibrary.Application.OperationData.Base.GetByIdQuery
+{
+    public interface IGetByIdQuery<TKey, TViewModel> : IRequest<Response<TViewModel>> where TKey: struct where TViewModel : class
+    {
+        TKey Id { get; set; }
+    }
+}
