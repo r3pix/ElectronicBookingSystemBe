81a03dd99dcfff1067e01a19877082fe9a2f7468
piotr.nowek <piotr.nowek@onwelo.com>
2022-08-19 15:49:49 +0200

    category fixes

---
+++ ElectronicLibrary.Application/Repositories/PageableRepository.cs
@@ -0,0 +1,31 @@
+﻿using ElectronicBookingSystem.Infrastructure.Extensions;
+using ElectronicLibrary.Domain.Entities;
+using ElectronicLibrary.Infrastructure.Extensions;
+using ElectronicLibrary.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicBookingSystem.Application.Repositories
+{
+    public abstract class SearchRepository<TFilter,TEntity>  where TEntity : BaseEntity where TFilter : class
+    {
+        private readonly ElectronicBookingSystemDbContext _dbContext;
+        protected DbSet<TEntity> dbSet;
+
+        public SearchRepository(ElectronicBookingSystemDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+            dbSet = dbContext.Set<TEntity>();       
+        }
+
+        public virtual async Task<(IEnumerable<TEntity>, int)> GetPageable(TFilter filter, IPageableQueryModel page) =>
+            await Filter(filter).Page(page);
+
+        protected virtual IQueryable<TEntity> Filter(TFilter filter) =>
+            dbSet;
+    }
+}
---
+++ ElectronicLibrary.Infrastructure/Extensions/QueryableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using ElectronicLibrary.Domain.Entities;
+using ElectronicLibrary.Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.DynamicLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicBookingSystem.Infrastructure.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public async static Task<(IEnumerable<TEntity>, int)> Page<TEntity>(this IQueryable<TEntity> query, IPageableQueryModel page) where TEntity : BaseEntity
+        {
+            var count = await query.CountAsync();
+            if (page.Desc)
+            {
+                query = query.OrderByDescending(x=>"x." + page.OrderBy);
+            }
+            else
+            {
+                query = query.OrderBy(x => "x." + page.OrderBy);
+            }
+            query = query.Skip((page.PageNumber - 1) * page.PageSize);
+            query.Take(page.PageSize);
+            return (await query.ToListAsync(), count);
+        }
+    }
+}
